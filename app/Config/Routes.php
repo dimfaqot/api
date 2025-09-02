fdc0c0ea0ef24b39fba8684e2543b772fa765a17
dimfaqot <dimyfaqot@gmail.com>
2025-09-02 15:42:00 +0700

    first commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/api/nota/by_no_nota/(:any)/(:any)', 'Nota::by_no_nota/$1/$2');
+$routes->get('/nota/by_no_nota/(:any)/(:any)', 'Nota::by_no_nota/$1/$2');
