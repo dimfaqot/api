ff85d55bf391fc7150d0552d9e2a4670f8cef06d
dimfaqot <dimyfaqot@gmail.com>
2025-12-26 21:29:06 +0700

    playground

--- app/Helpers/functions_helper.php
+++ app/Helpers/functions_helper.php
@@ -710,7 +710,7 @@ function transaksi($decode)
             ];
 
             if ($i['divisi'] == "Ps" || $i['divisi'] == "Billiard") {
-                $input['start'] = $tgl;
+                $input['start'] = ($i['metode'] == "Wl" ? $i['start'] : $tgl);
                 $input['end'] = ($input['qty'] == 0 ? 0 : $tgl + (int)$input['qty'] * (60 * 60));
                 $input['is_over'] = 0;
                 $input['roleplay'] = $i['roleplay'];
