6ac98ea26122f102c928bbbecd4a0a761d861d26
dimfaqot <dimyfaqot@gmail.com>
2026-01-18 23:10:56 +0700

    pg

--- app/Views/cetak/laporan_playground.php
+++ app/Views/cetak/laporan_playground.php
@@ -39,16 +39,16 @@
             </tr>
             <?php foreach ($rangkuman['data'] as $k => $i): ?>
                 <?php $rowspan = count($i['data']); ?>
-                <?php foreach ($i['data'] as $key => $d): ?>
-                    <tr>
-                        <td rowspan="<?= $rowspan; ?>" style="text-align:center;"><?= ($k + 1); ?></td>
-                        <td rowspan="<?= $rowspan; ?>"><?= $i['bulan']; ?></td>
+                <tr>
+                    <td rowspan="<?= $rowspan; ?>" style="text-align:center;"><?= ($k + 1); ?></td>
+                    <td rowspan="<?= $rowspan; ?>"><?= $i['bulan']; ?></td>
+                    <?php foreach ($i['data'] as $key => $d): ?>
                         <td><?= $d['divisi']; ?></td>
                         <td style="text-align: right;"><?= angka($d['masuk']); ?></td>
                         <td style="text-align: right;"><?= angka($d['keluar']); ?></td>
                         <td style="text-align: right;"><?= (((int)$d['total']) < 0 ? "- " : "") . angka($d['total']); ?></td>
-                    </tr>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+                </tr>
             <?php endforeach; ?>
             <tr>
                 <th colspan="5" style="text-align: center;">TOTAL</th>
