96dcc63bba450a39a9f97325eb24b45f5d31418e
dimfaqot <dimyfaqot@gmail.com>
2026-01-24 21:13:19 +0700

    api iot

--- app/Controllers/Iot.php
+++ app/Controllers/Iot.php
@@ -30,11 +30,11 @@ class Iot extends BaseController
         }
 
         if ($q['status'] == 1 && $q['end'] > 0 && $q['end'] < time()) {
-            $q['status'] = 0;
-            $q['end'] = 0;
-            $q['transaksi_id'] = 0;
+            $update['status'] = 0;
+            $update['end'] = 0;
+            $update['transaksi_id'] = 0;
 
-            if (!db('iot', $decode['db'])->where('id', $q['id'])->update($q)) {
+            if (!db('iot', $decode['db'])->where('id', $q['id'])->update($update)) {
                 gagal("Update status gagal");
             }
 
